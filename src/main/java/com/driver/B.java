8c3523268b68dc66537352c385f0a17836ccd19d
Rohitsah6372 <91113994+Rohitsah6372@users.noreply.github.com>
2024-08-13 10:04:47 +0530

    second commit

--- src/main/java/com/driver/B.java
+++ src/main/java/com/driver/B.java
@@ -2,7 +2,8 @@ package com.driver;
 
 public class B extends A{
 
-    public String meth(){
+    @Override
+    public String meth() {
         return "Method is overridden in Extended class B";
     }
 
--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,9 +3,6 @@ package com.driver;
 public class Main {
    public static void main(String[ ] args){
        B b = new B();
-
-       String ans = b.meth();
-       System.out.println(ans);
    }
   
 }
